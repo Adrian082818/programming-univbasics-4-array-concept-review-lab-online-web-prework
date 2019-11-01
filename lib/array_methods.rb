e31e189106ee9b10b31a6c49bfc01b0f6186173a
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-01 01:44:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   index = 0
  array.length.times do |index|
-  if array.index < value_to_find
+ array.index 
    index += 1
+   return index 
 end
 end
 end
