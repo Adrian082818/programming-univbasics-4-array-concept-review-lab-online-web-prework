fe153d7e99b17d0395af23bff7334d3732b2b6d8
Adrian Mohabir <mohabiradrian@gmail.com>
2019-11-01 01:51:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   while index < array.length do 
     puts array.index("value_to_find")
     index += 1
-    return index 
+    return value_to_find
   end 
 end
 
